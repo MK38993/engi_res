511a133a9e1d812b60d6c9a693cc405df059b705
MK38993 <matthewkwee@icloud.com>
2021-12-16 18:10:27 -0500

    Create webapp_wrapper.sh

---
+++ webapp_wrapper.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh 
+
+source ~/.zprofile
+source ~/.zshrc
+
+python webapp.py
+
+
+
+#http://127.0.0.1:5000/predict?bed_num=2&bath_num=2&sqft_num=2000&floor_num=2&room_num=6&year_num=2000&building_type=”Single-Family-Home”&borough=”Brooklyn”&description=Lovely+Description
